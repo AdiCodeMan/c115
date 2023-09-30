f33cf8d50ff8459b51dbfb706d5ca97fdf698691
AdiCodeMan <123280392+AdiCodeMan@users.noreply.github.com>
2023-09-29 17:16:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+var c;
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400,400);
+    canvas.center();
+    
+}
+
+function draw(){
+    background('white');
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
